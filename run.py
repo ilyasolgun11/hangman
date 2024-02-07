cc06dc9a4a1d09a98465fbf0b035c66d5ad669a3
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-02-07 23:27:48 +0000

    Fix overflow issue with print statement

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ class HangmanGame:
 
     def player_info(self):
         print(self.game_logo)
-        print(Fore.YELLOW + "Welcome stranger!! could you be the one to save this poor guy from a gruesome death? i hope so! fill in your name and location to begin.\n")
+        print(Fore.YELLOW + "Welcome stranger!! could you be the one to save this poor guy from a \ngruesome death? i hope so! fill in your name and location to begin.\n")
         self.name_of_player = input(Fore.BLUE + "What is your name?\n>>> ")
         self.location_of_player = input(Fore.BLUE + "Which country are your from?\n>>> ")
         self.how_to_play()

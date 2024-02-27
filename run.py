2b0a8f2c3e61bff94b025d35f85c31c22b555bfc
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-02-27 18:35:12 +0000

    Add while statement to collect_info function to display error when user does not input anything

--- run.py
+++ run.py
@@ -46,11 +46,22 @@ class PlayerInfo:
         """
         print(self.game_logo)
         print(Fore.YELLOW + "Welcome stranger! could you be the one to save this poor guy from a \ngruesome death? i hope so! fill in your name and location to\nsee the how to play guide.\n")
-        self.name_of_player = input(
-            Fore.CYAN + "What is your first name?\n>>> ")
-        self.location_of_player = input(
-            Fore.CYAN + "Which country/city are your from?\n>>> ")
-        self.how_to_play()
+        while True:
+            self.name_of_player = input(
+                Fore.CYAN + "What is your first name?\n>>> ")
+            if len(self.name_of_player) == 0:
+                print(Fore.YELLOW + "Please enter your name..")
+            else:
+                break
+
+        while True:
+            self.location_of_player = input(
+                Fore.CYAN + "Which country/city are your from?\n>>> ")
+            if len(self.location_of_player) == 0:
+                print(Fore.YELLOW + "Please enter your name..")
+            else:
+                self.how_to_play()
+                break
 
 
 class HangmanGame(PlayerInfo):

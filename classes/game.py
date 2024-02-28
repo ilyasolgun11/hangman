ab8d7a10be94ea696a0d7fba6c2e27b65b774e90
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-02-28 18:05:11 +0000

    Add append_to_worksheet and get_leaderboard_data function to leaderboard class and link it up to game.py

--- classes/game.py
+++ classes/game.py
@@ -1,5 +1,6 @@
 from player import Player
 from leaderboard import Leaderboard
+from words import RandomWord
 import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
@@ -8,19 +9,21 @@ import requests
 from datetime import datetime
 import time
 from dotenv import load_dotenv
+import sys
 import os
 load_dotenv(dotenv_path='env.py')
 api_key = os.getenv('API_KEY')
 
-class Game(Player, Leaderboard):
+class Game(Player, Leaderboard, RandomWord):
 
     def __init__(self):
         super().__init__()
         self.start_time = None
         self.player_won = False
         self.selected_worksheet = "easy mode"
-        self.hangman_word = self.game_modes("easy mode")
-        self.display_word = "_" * len(self.game_modes("easy mode"))
+        self.random_word_instance = RandomWord()
+        self.hangman_word = self.random_word_instance.game_modes("easy mode")
+        self.display_word = "_" * len(self.random_word_instance.game_modes("easy mode"))
         self.hangman_stage = hangman_stages
         self.stages = 0
         self.game_logo = hangman_logo
@@ -70,20 +73,20 @@ class Game(Player, Leaderboard):
                 Fore.WHITE +
                 """Type 'a', 'b' or 'c' below\n>>> """)
             if player_mode_option.lower() == "a":
-                self.hangman_word = self.game_modes("easy mode")
-                self.display_word = "_" * len(self.game_modes("easy mode"))
+                self.hangman_word = self.random_word_instance.game_modes("easy mode")
+                self.display_word = "_" * len(self.random_word_instance.game_modes("easy mode"))
                 self.selected_worksheet = "easy mode"
                 self.play()
                 break
             elif player_mode_option.lower() == "b":
-                self.hangman_word = self.game_modes("intermediate mode")
-                self.display_word = "_" * len(self.game_modes("intermediate mode"))
+                self.hangman_word = self.random_word_instance.game_modes("intermediate mode")
+                self.display_word = "_" * len(self.random_word_instance.game_modes("intermediate mode"))
                 self.selected_worksheet = "intermediate mode"
                 self.play()
                 break
             elif player_mode_option.lower() == "c":
-                self.hangman_word = self.game_modes("hard mode")
-                self.display_word = "_" * len(self.game_modes("hard mode"))
+                self.hangman_word = self.random_word_instance.game_modes("hard mode")
+                self.display_word = "_" * len(self.random_word_instance.game_modes("hard mode"))
                 self.selected_worksheet = "hard mode"
                 self.play()
                 break
@@ -212,6 +215,156 @@ class Game(Player, Leaderboard):
                                hints_used]
                 self.append_to_worksheet(self.selected_worksheet, data_to_add)
                 self.game_end()
+                
+    def guess_word(self, user_input):
+        """
+        Checks if the user word input is correct or not, increments or decrements points accordingly
+        """
+        # If the user has guessed a word, and the word was already guessed,
+        # display message
+        if user_input in self.guessed_words:
+            self.game_hint_message = Fore.YELLOW + \
+                f"You have guessed the word '{user_input}' already."
+        else:
+            # If the user_input is equal to the hangman_word, check if the user guessed the word before or after half the letters were found
+            # already, if they guessed before they revealed the first half of
+            # letters then reward them with 750 points, otherwise reward them
+            # with 100
+            if user_input == self.hangman_word:
+                if len(self.guessed_correct_letters) < round(
+                        len(self.hangman_word) / 2):
+                    self.points += 750
+                    self.player_won = True
+                else:
+                    self.points += 100
+                    self.player_won = True
+            # If the word the user guessed is not equal to the hangman word
+            # then take away all their points, and decrement the attempts by 1
+            else:
+                self.points = 0
+                self.score -= 1
+                self.guessed_words.append(user_input)
+                self.game_hint_message = Fore.RED + \
+                    f"Wrong! {user_input} is not the word"
+                # If the attempts left is not 0 then increment the stages
+                # attribute to get the next ASCII art from the hangman_stages
+                # list
+                if self.score != 0:
+                    self.stages += 1
+                else:
+                    pass
+
+    def guess_letter(self, user_input):
+        """
+        Checks if user letter input is correct or not, increments or decrements points accordingly
+        """
+        # If the user input letter was already guessed, ask the user to
+        # re-enter a letter
+        if user_input in self.guessed_letters or user_input in self.guessed_correct_letters:
+            self.game_hint_message = Fore.YELLOW + \
+                f"You have guessed the letter '{user_input}' already"
+        else:
+            # If the letter the user guessed is in the hangman word do the
+            # following..
+            if user_input in self.hangman_word:
+                # If the user input letter is in the hangman word then add 25
+                # points and congratulate the user
+                if user_input in self.hangman_word:
+                    self.guessed_correct_letters.append(user_input)
+                    self.update_display_word(user_input)
+                    self.game_hint_message = Fore.GREEN + \
+                        f"Correct! the letter '{user_input}' is in the word!"
+                    self.points += 25
+                # If there is no more underscores left in the display_word
+                # string, then let the user know they won
+                if "_" not in self.display_word:
+                    print(f"You have won! the word is {self.hangman_word}")
+                    self.player_won = True
+            else:
+                # If the user input letter is not in the hangman word then check if the points is under 10, if it is not then
+                # decrement by 10 and decrement the attempts left.
+                if self.points < 10:
+                    pass
+                else:
+                    self.points -= 10
+                self.score -= 1
+                self.guessed_letters.append(user_input)
+                self.game_hint_message = Fore.RED + \
+                    f"Wrong! the letter {user_input} is not in the word"
+                # If the attempts left is not 0 then increment the stages
+                # attribute to get the next ASCII art from the hangman_stages
+                # list
+                if self.score != 0:
+                    self.stages += 1
+                else:
+                    pass
+
+    def update_display_word(self, user_input):
+        """
+        Checks if the user letter input is in the self.hangman_word, if it is it reveals
+        the letter in the correct place
+        """
+        updated_display = ""
+        # Hide the hangman word with underscores, if the user guesses a letter
+        # right then reveal the letters in the correct index
+        for winning_word, displayed_word in zip(self.hangman_word, self.display_word):
+            if winning_word == user_input or displayed_word != "_":
+                updated_display += winning_word if winning_word == user_input else displayed_word
+            else:
+                updated_display += "_"
+        self.display_word = list(updated_display)
+
+    def reset_game(self):
+        """
+        Resets only the class attributes that need to be reset, leaves the user name and location the same
+        """
+        self.player_won = False
+        self.hangman_word = self.random_word_instance.game_modes("easy mode")
+        self.display_word = "_" * len(self.random_word_instance.game_modes("easy mode"))
+        self.stages = 0
+        self.score = 7
+        self.points = 0
+        self.guessed_letters = []
+        self.guessed_words = []
+        self.guessed_correct_letters = []
+        self.game_hint_message = ""
+        self.hints_remaining = 1
+
+    def game_end(self):
+        """
+        Displays win or lose screen depending if the user won or not, also asks the user if they
+        want to play again, check leaderboard or exit the game
+        """
+        if self.player_won:
+            print(self.win_logo)
+            print(Fore.GREEN + f"Amazing job! you saved him!\n")
+            print(Fore.YELLOW + "Leaderboard's updated.\n")
+        else:
+            print(self.lose_logo)
+            print(Fore.RED + "Better luck next time, my dude is dead!\n")
+
+        print(f"The word was " + Fore.CYAN + f"{self.hangman_word}\n")
+        print(f"Points: {self.points}\n")
+        # While the user does not select an invalid option (anything other than
+        # a, b or c) then keep asking them for a valid input
+        while True:
+            user_choice = input(
+                "A - Play again\nB - Exit game\nC - Leaderboard\nType 'a', 'b' or 'c' below\n>>>")
+            if user_choice.lower() == "a":
+                self.reset_game()
+                self.choose_game_mode()
+                break
+            elif user_choice.lower() == "b":
+                print(f"Thanks for playing {self.name_of_player}!")
+                print("Hangman awaits your return!")
+                sys.exit()
+            elif user_choice.lower() == "c":
+                self.get_leaderboard_data()
+                self.reset_game()
+                break
+            else:
+                print(Fore.YELLOW + "Please enter a valid option.")
+
    
 if __name__ == "__main__":
     game = Game()
--- classes/leaderboard.py
+++ classes/leaderboard.py
@@ -1,5 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+from hangman import *
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,5 +19,52 @@ SPREADSHEET_NAME = 'ultimate-hangman-leaderboard'
 SHEET = GSPREAD_CLIENT.open(SPREADSHEET_NAME)
 
 class Leaderboard:
+    def __init__(self):
+        self.selected_worksheet = "easy mode"
+
     def append_to_worksheet(self, sheet, data):
         return SHEET.worksheet(sheet).append_row(data)
+    
+    def get_leaderboard_data(self):
+        """
+        Gets leaderboard data from google sheets and displays the top 15 highest scores, also gives user the option
+        to play again, choose a different modes leaderboard or exit the game
+        """
+        leaderboard_sheet = SHEET.worksheet(self.selected_worksheet)
+        leaderboard_data = leaderboard_sheet.get_all_records()
+        sorted_leaderboard = sorted(
+            leaderboard_data,
+            key=lambda x: x['Points'],
+            reverse=True)
+        if self.selected_worksheet == "easy mode":
+            leaderboard_header_mode = "E A S Y   M O D E"
+        elif self.selected_worksheet == "intermediate mode":
+            leaderboard_header_mode = "I N T E R M E D I A T E    M O D E"
+        elif self.selected_worksheet == "hard mode":
+            leaderboard_header_mode = "H A R D    M O D E"
+        print(
+            Fore.YELLOW +
+            "--------------------------------------------------------------------------------")
+        print(
+            Fore.YELLOW +
+            f"  T O P   1 5   L E A D E R B O A R D  |  {leaderboard_header_mode}")
+        print(
+            Fore.YELLOW +
+            "--------------------------------------------------------------------------------\n")
+        print(
+            Fore.BLUE +
+            "POSITION  NAME      POINTS LOCATION    DATE        TIME TO WIN    WORD        HINT USED?")
+        # Using the data from worksheet.get_all_records(), the data is displayed with designated column widths to separate
+        # columns evenly without overflow
+        for position, player_data in enumerate(
+                sorted_leaderboard[:15], start=1):
+            position_str = str(position).ljust(10)
+            name_str = player_data['Name'].ljust(10).capitalize()
+            points_str = str(player_data['Points']).ljust(7)
+            location_str = player_data['Country/City'].ljust(12).capitalize()
+            date_str = player_data['Date'].ljust(12)
+            time_to_win_str = player_data['Time to win'].ljust(15)
+            winning_word = player_data['Winning word'].ljust(12).capitalize()
+            hint_used = player_data['Hint used?'].ljust(12).capitalize()
+            print(Fore.GREEN + f"""{position_str}{name_str}{points_str}{location_str}{
+                  date_str}{time_to_win_str}{winning_word}{hint_used}""")

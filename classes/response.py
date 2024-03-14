ac15fc412e77aa2e1077703f0f975a834c998b8c
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-03-14 19:55:52 +0000

    Remove unnessasary comments from response.py file

--- classes/response.py
+++ classes/response.py
@@ -16,16 +16,8 @@ class GrabResponse:
     @staticmethod
     def grab_word_definition(word):
         print(Fore.LIGHTYELLOW_EX + "Grabbing definition...")
-        # URL in which would be used to gather the definition
-        # data for the self.hangman_word if the user decides
-        # to use the hint token
         url = f"https://dictionary-data-api.\
 p.rapidapi.com/definition/{word}"
-        # Header that would be used alongside with the url
-        # to gather definition data of
-        # the self.hangman_word, and holds the secret api
-        # key which is being held
-        # in the env.py file
         headers = {'X-RapidAPI-Key': api_key}
         response = requests.get(url, headers=headers)
         # If the data is grabbed successfully, do the following
@@ -35,16 +27,11 @@ p.rapidapi.com/definition/{word}"
                 meanings = data.get('meaning', [])
                 if meanings:
                     definition = meanings[0]["values"][0]
-                    # Display definition to player and hide
-                    # the word by replacing
-                    # it with "(hidden correct word)"
                     print(Fore.LIGHTCYAN_EX + f"Definition of word: \
 {definition.replace(word, '(hidden correct word)')}" + Fore.RESET)
                 else:
-                    # If no definition is found, let the player know
                     print("No meanings found.")
             else:
-                # If no data has been grabbed, let the player know
                 print(f"Error: Sorry, no definitions found...")
         except Exception as e:
             # Handle any exception that may occur
@@ -53,15 +40,8 @@ p.rapidapi.com/definition/{word}"
     @staticmethod
     def grab_country_data(country):
         print(Fore.LIGHTYELLOW_EX + "Grabbing country data...")
-        # URL in which would be used to gather the definition
-        # data for the self.hangman_word
-        # if the user decides to use the hint token
         url = f"https://geography4.p.rapidapi.com/apis\
 /geography/v1/country/name/{country}"
-        # Header that would be used alongside with the url
-        # to gather definition data of
-        # the self.hangman_word, and holds the secret api
-        # key which is being held in the env.py file
         headers = {'X-RapidAPI-Key': api_key}
         response = requests.get(url, headers=headers)
         # If the data is grabbed successfully, do the following
